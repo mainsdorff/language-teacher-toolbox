03f43aec0d3b2155e7be0ae103c9fbd3d8662470
mainsdorff <moiseyev.k@gmail.com>
2022-07-29 11:06:01 +0700

    Updating Word Jumbler

--- word_jumbler/script.js
+++ word_jumbler/script.js
@@ -1,1 +1,9 @@
 const originalWord = document.querySelector("#input");
+const jumbleButton = document.querySelector("#jumble-button");
+
+jumbleButton.addEventListener("click", jumbleWord(originalWord.value));
+
+const jumbleWord = word => {
+  let jumbledWord = "";
+
+}
